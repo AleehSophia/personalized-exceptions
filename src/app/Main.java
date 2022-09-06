2b3365fe1dcab862ab30c8404a64f862255e7de9
AleehSophia <alessia.casado@gmail.com>
2022-09-06 12:44:32 -0300

    :sparkles:feat: implemented in main output program

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,29 @@
 package app;
 
+import models.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Room number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+        Reservation reservation = new Reservation(n, checkIn, checkOut);
+        System.out.print("Reservation: " + reservation);
+
+        sc.close();
     }
 }
