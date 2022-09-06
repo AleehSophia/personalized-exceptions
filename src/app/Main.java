c93de47badc0916a335c80df338223093cc14627
AleehSophia <alessia.casado@gmail.com>
2022-09-06 13:02:30 -0300

    :sparkles:feat: Adding update dates in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -14,15 +14,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Room number: ");
-        int n = sc.nextInt();
+        int number = sc.nextInt();
 
         System.out.print("Check-in date (dd/MM/yyyy): ");
         Date checkIn = sdf.parse(sc.next());
 
         System.out.print("Check-out date (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
-        Reservation reservation = new Reservation(n, checkIn, checkOut);
+        Reservation reservation = new Reservation(number, checkIn, checkOut);
         System.out.print("Reservation: " + reservation);
+
+        System.out.println();
+        System.out.print("Enter data to update the reservation:");
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (dd/MM/yyyy): ");
+        checkOut = sdf.parse(sc.next());
+
+        reservation.updateDates(checkIn, checkOut);
+        System.out.println("Reservation:" + reservation);
 
         sc.close();
     }
